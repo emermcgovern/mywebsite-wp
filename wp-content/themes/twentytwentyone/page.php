a6f0ad3343922638f42fc2b0c0a91bf466d61f5c
emermcgovern <79218759+emermcgovern@users.noreply.github.com>
2021-05-25 14:51:18 +0100

    Update page.php

--- wp-content/themes/twentytwentyone/page.php
+++ wp-content/themes/twentytwentyone/page.php
@@ -1,25 +1,38 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package my_website_2
  */
 
 get_header();
+?>
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
+	<main id="primary" class="site-main">
 
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
+		<?php
+		while ( have_posts() ) :
+			the_post();
 
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
 get_footer();
