8de62c51e44a6d276b8970819b8c90c88f4650d4
emermcgovern <79218759+emermcgovern@users.noreply.github.com>
2021-05-25 16:23:59 +0100

    Update footer.php

--- wp-content/themes/my-website-2/footer.php
+++ wp-content/themes/my-website-2/footer.php
@@ -6,24 +6,14 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package my_website_2
+ * @package My_Website
  */
 
 ?>
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'my-website-2' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'my-website-2' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'my-website-2' ), 'my-website-2', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>Copyright &copy; 2021 My Website Inc. 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
