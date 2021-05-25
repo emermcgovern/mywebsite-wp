f9b83cf13931c8a4792e745655e9ed8d0527c9e4
emermcgovern <79218759+emermcgovern@users.noreply.github.com>
2021-05-25 14:41:54 +0100

    wp

--- wp-content/themes/my-website-2/page.php
+++ wp-content/themes/my-website-2/page.php
@@ -1,35 +1,26 @@
 <?php
-/*
- * The default template for displaying pages.
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
  */
-?>
 
-<?php get_header(); ?>
-<div id="content" role="main">
-	<?php while ( have_posts() ) : the_post(); ?>
+get_header();
 
-		<h1 class="page-title"><?php the_title(); ?></h1>
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content', 'page' );
 
-		<div class="entry-content-page">
-			<?php if ( has_post_thumbnail() ) {
-				the_post_thumbnail('post-thumbnail', array('class' => 'single-image'));
-			} ?>
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop.
 
-			<?php the_content(); ?>
-
-			<?php wp_link_pages( array(
-				'before' => '<div class="pagelink">' . __( 'Pages:', 'darkorange' ),
-				'after'  => '</div>',
-			) ); ?>
-		</div>
-
-		<?php comments_template(); ?>
-
-	<?php endwhile; ?>
-
-	<?php edit_post_link( __( 'Edit', 'darkorange' ), '<div class="edit-link">', '</div>' ); ?>
-</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
-
+//get_footer();
 
