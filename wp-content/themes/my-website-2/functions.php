1af0a0b0271aecf1b971be8021aa87ac12cc1513
emermcgovern <79218759+emermcgovern@users.noreply.github.com>
2021-05-25 12:50:00 +0100

    Update functions.php

--- wp-content/themes/my-website-2/functions.php
+++ wp-content/themes/my-website-2/functions.php
@@ -13,32 +13,16 @@ if ( ! defined( '_S_VERSION' ) ) {
 }
 
 if ( ! function_exists( 'my_website_2_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
+	
 	function my_website_2_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on my website 2, use a find and replace
-		 * to change 'my-website-2' to the name of your theme in all the template files.
-		 */
+		
 		load_theme_textdomain( 'my-website-2', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+	
+		//add_theme_support( 'title-tag' );
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
