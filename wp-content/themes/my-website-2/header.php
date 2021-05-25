88febf5afa9f1c3eec00f93278d26fca3e498273
emermcgovern <79218759+emermcgovern@users.noreply.github.com>
2021-05-25 14:37:11 +0100

    Update header.php

--- wp-content/themes/my-website-2/header.php
+++ wp-content/themes/my-website-2/header.php
@@ -1,53 +1,26 @@
-<?php
-/**
- * The template for displaying the header
- *
- * Displays all of the head element and everything up until the page header div.
- *
- * @package Hestia
- * @since Hestia 1.0
- */
-$wrapper_div_classes = 'wrapper ';
-if ( is_single() ) {
-	$wrapper_div_classes .= join( ' ', get_post_class() );
-}
+="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$my_website_2_description = get_bloginfo( 'description', 'display' );
+			if ( $my_website_2_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $my_website_2_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
 
-$layout               = apply_filters( 'hestia_header_layout', get_theme_mod( 'hestia_header_layout', 'default' ) );
-$disabled_frontpage   = get_theme_mod( 'disable_frontpage_sections', false );
-$wrapper_div_classes .=
-	(
-		( is_front_page() && ! is_page_template() && ! is_home() && false === (bool) $disabled_frontpage ) ||
-		( class_exists( 'WooCommerce', false ) && ( is_product() || is_product_category() ) ) ||
-		( is_archive() && ( class_exists( 'WooCommerce', false ) && ! is_shop() ) )
-	) ? '' : ' ' . $layout . ' ';
-
-$header_class = '';
-$hide_top_bar = get_theme_mod( 'hestia_top_bar_hide', true );
-if ( (bool) $hide_top_bar === false ) {
-	$header_class .= 'header-with-topbar';
-}
-
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-
-<head>
-	<meta charset='<?php bloginfo( 'charset' ); ?>'>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php endif; ?>
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
-	<div class="<?php echo esc_attr( $wrapper_div_classes ); ?>">
-		<header class="header <?php echo esc_attr( $header_class ); ?>">
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'my-website-2' ); ?></button>
 			<?php
-			hestia_before_header_trigger();
-			do_action( 'hestia_do_top_bar' );
-			do_action( 'hestia_do_header' );
-			hestia_after_header_trigger();
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
 			?>
-		</header>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
