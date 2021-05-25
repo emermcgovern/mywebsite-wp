8b5f435603af0fff2b5021178ee2908d206bcf83
emermcgovern <79218759+emermcgovern@users.noreply.github.com>
2021-05-25 14:17:46 +0100

    Update header.php

--- wp-content/themes/my-website-2/header.php
+++ wp-content/themes/my-website-2/header.php
@@ -8,20 +8,16 @@
  *
  * @package my_website_2
  */
-?><!DOCTYPE html>
 
-<html class="no-js" <?php language_attributes(); ?>>
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
 
-	<head>
-
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-
-		<link rel="profile" href="https://gmpg.org/xfn/11">
-
-		<?php wp_head(); ?>
-
-	</head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
